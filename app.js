997578a5cc17db03fabfe19be0af2cec826767ce
Jeff Hall <jeffreyrhall@gmail.com>
2015-12-05 13:54:43 -0500

    Should now be able to get the dmg with some nicer looking output while processing.

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var exec_string = ' ' + 'hdiutil create -format UDZO ' +
 ;
 
 console.log();
-console.log( 'STARTING: ' + exec_string );
+console.log( 'Creating a dmg called \'' + targetName + '\' from \'' + sourceFolder +'\'.' );
 console.log();
 
 /*--------
@@ -39,8 +39,55 @@ var Parse_puppetstrings = function() {
 };
 inherits( Parse_puppetstrings , Transform ) ;
 
+var hasStartedInit = false ;
+var hasStarted  = false ;
+
 Parse_puppetstrings.prototype._transform = function( chunk, enc, onDone ){
-  this.push( chunk );
+  var els = chunk.toString().split(':');
+  var type = els[0] ;
+  var value = els[1] ;
+  var output;
+
+  
+  if( type==='PERCENT' ) {
+      //
+      // The first -1 should alert the user that we are initializing.
+      // After that, any initializations should just add a '.' to let
+      // the user know that we're still working.
+      // When we have a number to report to the user, report it.
+      // Once we've started, we know that if we get another -1, we must
+      // be winding down.
+      // If the type isn't a percent, then we know that we are done or that
+      // there is a error.
+      //
+
+      var value = parseInt( value ) ;
+      if ( value === -1 && hasStartedInit === false ) {
+        hasStartedInit = true ;
+        output = 'Initializing...' ;
+      }
+      else if( 
+       value === -1 && 
+       hasStartedInit === true && 
+       hasStarted === false 
+      ) {
+        output = '.' ;
+      }
+      else if( value !== -1 ) {
+        hasStarted = true ; 
+        output = '\n' + parseInt(value) ;
+      }
+      else if( value === -1 && hasStarted ) {
+        output = '\n' + 'Finishing up...' ;
+      }
+      else {
+        output = value ;
+      }
+  }
+  else if( type === 'created' ) {
+      output = '\n' + 'done' + '\n' ;
+  }
+  this.push( output );
   onDone();  
 };
 
@@ -48,5 +95,4 @@ command.stdout
   .pipe( new Parse_puppetstrings )
   .pipe(process.stdout) ;
 command.stderr
-  .pipe( new Parse_puppetstrings )
   .pipe(process.stdout) ;
