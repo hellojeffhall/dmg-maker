3384379857615a4d680f9ff50a75544fd9adfaa4
Jeff Hall <jeffreyrhall@gmail.com>
2015-12-05 21:59:34 -0500

    We now have basic selection of the source and target paths.

--- js/script.js
+++ js/script.js
@@ -3,23 +3,52 @@ const dialog = remote.require('dialog');
 const BrowserWindow = remote.BrowserWindow ;
 const mainWindow = BrowserWindow.getFocusedWindow() ;
 
-var choose_source = function(){
-  
+// Functions
+
+var choose_directory = function( callback ){
   dialog.showOpenDialog( 
     mainWindow ,
     { 
-      'title' : 'Please choose the source folder.' ,
       'properties' : ['openDirectory']             ,
     } , 
     function( array_of_filepaths ){
       var result = array_of_filepaths[0] ;
-      document.querySelector('.source_path').innerHTML = result ;
+      callback( result ) ; 
     }
   );
 };
 
+var set_target = function( callback ){
+};
+
+// I/O
+
+var default_target_path ;
+
 document.querySelector('.source_selector')
   .addEventListener('click' , function(ev) {
-    choose_source() ;
+    choose_directory( function (result) {
+      if ( result !== undefined ) {
+        document.querySelector('.source_path').innerHTML = result ;
+        default_target_path = result + '.dmg';
+      }
+    }) ;
+  })
+;
+
+document.querySelector('.target_selector')
+  .addEventListener('click' , function(ev) {
+    dialog.showSaveDialog( 
+      mainWindow ,
+      { 
+        'properties'  : ['openDirectory' ]  ,
+        'defaultPath' : default_target_path 
+      } , 
+      function( result ){
+        if ( result !== undefined ) {
+          document.querySelector('.target_path').innerHTML = result ;
+        };
+      }
+    );
   })
 ;
